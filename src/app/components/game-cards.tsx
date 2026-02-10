77d1e106540ef6491c1bc7a73e082802236d3a68
luiz043 <lh864038@gmail.com>
2026-02-10 19:52:48 -0300

    add carousel

--- src/app/components/game-cards.tsx
+++ src/app/components/game-cards.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { useState, useEffect } from "react"
+import { Carousel } from "primereact/carousel"
+
 export default function GameCards() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
-  const apiUrl = "https://api.pexels.com/v1/curated?page=1&per_page=10"
+  const apiUrl = "https://api.pexels.com/v1/curated?page=1&per_page=30"
   const apiKey = "SmFsJEiD2IlwQtcdDfFkXxMHRRjPgaZ6q5lDDRQ4RNAGVM4JJCaYMI6y"
 
   const requestOptions = {
@@ -16,6 +18,7 @@ export default function GameCards() {
   useEffect(() => {
     async function fetchData() {
       try {
+        setLoading(true)
         const response = await fetch(apiUrl, requestOptions)
         const json = await response.json()
         setData(json)
@@ -24,20 +27,46 @@ export default function GameCards() {
       }
     }
     fetchData()
+    setLoading(false)
   }, [])
 
-  const removed = data?.photos.filter((img, index) => index != 9 && index != 8)
+  const removed = data?.photos
+
+  const template = (img) => {
+    return (
+      <img
+        className="rounded-xl  shadow-2xl border-2 border-green-500"
+        key={img.id}
+        src={img.src.tiny}
+        alt={img.alt}
+      />
+    )
+  }
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-8 gap-5 m-5  ">
-      {removed?.map((img) => (
-        <img
-          className="rounded-xl shadow-2xl border-2 border-green-500"
-          key={img.id}
-          src={img.src.tiny}
-          alt={img.alt}
-        />
-      ))}
+    // <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-8 gap-5 m-5  ">
+    // <div className="flex  gap-5 m-5  ">
+    //   {removed?.map((img) => (
+    //     <img
+    //       className="rounded-xl  shadow-2xl border-2 border-green-500"
+    //       key={img.id}
+    //       src={img.src.tiny}
+    //       alt={img.alt}
+    //     />
+    //   ))}
+    // </div>
+    <div className="flex ">
+      <Carousel
+        pt={{
+          previousButton: { className: "size-10" },
+          nextButton: { className: "size-10" },
+        }}
+        circular
+        value={data?.photos}
+        numVisible={6}
+        itemTemplate={template}
+        numScroll={6}
+      />
     </div>
   )
 }
